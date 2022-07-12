b115ae2607fce63d0b069b6d81f616da49c95aed
Edwin Boon <edwin.boon@eo.nl>
2022-07-12 11:32:11 +0200

    Implemented both easy and hard level for password generator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Password Generator Project
-import random
+from random import choice, shuffle
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -9,9 +10,22 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+password = ''
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+for letter in range(0, nr_letters):
+  password += choice(letters)
 
+for symbol in range(0, nr_symbols):
+  password += choice(symbols)
+
+for number in range(0, nr_numbers):
+  password += choice(numbers)
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+shuffled_password = list(password)
+
+shuffle(shuffled_password)
+
+print(''.join(shuffled_password))
